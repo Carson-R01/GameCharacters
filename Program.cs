ca1025e59d5b4bf3572f32d2ee5470487b3c45ac
carsonreiter <creiter3@my.wctc.edu>
2025-03-27 08:47:52 -0500

    [3.02] Create Edit Dk

--- Program.cs
+++ Program.cs
@@ -171,6 +171,28 @@ do
   } else {
     logger.Info("Invalid choice");
   }
+  if (choice == "8") // Edit Donkey Kong Character
+{
+    Console.WriteLine("Enter the ID of the Donkey Kong character to edit:");
+    if (UInt32.TryParse(Console.ReadLine(), out UInt32 Id))
+    {
+        DonkeyKong? character = dkCharacters.FirstOrDefault(c => c.Id == Id);
+        if (character == null)
+        {
+            logger.Error($"Character Id {Id} not found");
+        }
+        else
+        {
+            InputCharacter(character);
+            File.WriteAllText(dkFile, JsonSerializer.Serialize(dkCharacters));
+            logger.Info($"Character Id {Id} edited");
+        }
+    }
+    else
+    {
+        logger.Error("Invalid Id");
+    }
+}
       if (choice == "9")
   {
     // Display Sf2 Characters
